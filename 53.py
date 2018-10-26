907124851f67cec733e7395ab20356acab811d39
lsh000 <43438948+lsh000@users.noreply.github.com>
2018-10-26 23:45:50 +0800

    Update 53.py

--- 53.py
+++ 53.py
@@ -9,12 +9,12 @@ y = month - 1
 z = day - 1
 dic = {0:0,1:31,2:59,3:90,4:120,5:151,6:181,7:212,8:243,9:273,10:304,11:334}
 if x != 0:
+    import math
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        
         if y >= 1:
-            r = x*(365)+(int(x/4))+dic[y]+z+1    
+            r = x*(365)+math.ceil(x/4)+dic[y]+z+1    
     else:
-        r = x*(365)+(int(x/4))+dic[y]+z
+        r = x*(365)+math.ceil(x/4)+dic[y]+z
 else:
     if y >= 1:
         r=dic[y]+z+1
